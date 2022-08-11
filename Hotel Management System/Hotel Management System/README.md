940ba8362aa886dbfcc79d21d6b34d20d95f7b04
ganeshwankhade <93642515+ganeshwankhade@users.noreply.github.com>
2022-08-11 22:47:21 +0530

    Add files via upload

---
+++ Hotel Management System/Hotel Management System/README.md
@@ -0,0 +1,4 @@
+
+Here Project is based on Python GUI using Tkinter(PAGE SOFTWARE) and SQLite3.
+Although PAGE software is not required to run program. Download all files including support module only python 3 is required.
+For viewing content in database use DB Browser(SQLite) (It is tool for SQLite)and open databse 'Project.sqlite' form provided file.
---
+++ Hotel Management System/Hotel Management System/register_support.py
@@ -0,0 +1,40 @@
+
+import sys
+
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
+
+try:
+    import ttk
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = True
+
+def set_Tk_var():
+    global gender
+    gender = tk.StringVar()
+    global Type
+    Type = tk.StringVar()
+
+def init(top, gui, *args, **kwargs):
+    global w, top_level, root
+    w = gui
+    top_level = top
+    root = top
+
+def destroy_window():
+    # Function which closes the window.
+    global top_level
+    top_level.destroy()
+    top_level = None
+
+if __name__ == '__main__':
+    import register
+    register.vp_start_gui()
+
+
+
+
